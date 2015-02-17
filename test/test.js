411ea6ad2549486aa98f9893eaa3277aed7e4d5d
zsx <zsx@zsxsoft.com>
2015-02-17 20:12:18 +0800

    Add Unittest

--- test/test.js
+++ test/test.js
@@ -1,5 +1,19 @@
-exports.testSomething = function (test) {
+exports.testUserAgents = function(test) {
 	var userAgent = require('../index.js');
-	console.log(userAgent.analyze('test'));
+	var testUAList = require('./ualist.js');
+
+	for (var index in testUAList) {
+		var item = testUAList[index];
+		var ua = userAgent.analyze(item[0][0]);
+		test.equals(item[1][2], ua.browser.full, ua.ua);
+		test.equals(item[1][3], ua.platform.full, ua.ua);
+		/*if (item[1][3] != ua.platform.full) {
+			console.log(ua.ua);
+			console.log(item[1][3]);
+			console.log(ua.platform.full);
+			console.log("");
+		}*/
+	}
+
 	test.done();
 }
