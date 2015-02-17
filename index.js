2ebdbd742d226ca23f015d214e0888af01d65f5c
zsx <zsx@zsxsoft.com>
2015-02-17 20:12:05 +0800

    =.=

--- index.js
+++ index.js
@@ -1,47 +1,55 @@
 (function() {
 	"use strict";
+
+	var NODE = typeof module !== 'undefined' && module.exports;
+	var CMD = typeof define !== 'undefined' && define.cmd;
+	var AMD = typeof define !== 'undefined' && define.amd;
 	var userAgent = {};
-
-
-	if (typeof require === 'undefined') {
-		if (typeof USERAGENT_OS !== 'undefined') var osDetect = new USERAGENT_OS();
-		if (typeof USERAGENT_DEVICE !== 'undefined') var deviceDetect = new USERAGENT_DEVICE();
-		if (typeof USERAGENT_BROWSER !== 'undefined') var browserDetect = new USERAGENT_BROWSER();
-	} else {
-		var osDetect = require('./lib/os');
-		var deviceDetect = require('./lib/device');
-		var browserDetect = require('./lib/browser');
-	}
-
 
 	userAgent.analyze = function(uaString) {
 		var returnObject = {};
 		returnObject.ua = uaString;
-		if (typeof osDetect !== 'undefined') returnObject.os = osDetect.analyze(uaString);
-		if (typeof deviceDetect !== 'undefined') returnObject.device = deviceDetect.analyze(uaString);
-		if (typeof browserDetect !== 'undefined') returnObject.browser = browserDetect.analyze(uaString);
+		if (typeof this.osDetect !== 'undefined') returnObject.os = this.osDetect.analyze(uaString);
+		if (typeof this.deviceDetect !== 'undefined') returnObject.device = this.deviceDetect.analyze(uaString);
+		if (typeof this.browserDetect !== 'undefined') returnObject.browser = this.browserDetect.analyze(uaString);
 		if (typeof returnObject.device !== 'undefined') returnObject.platform = returnObject.device;
 		if (typeof returnObject.os !== 'undefined' && returnObject.device.name == "") returnObject.platform = returnObject.os;
 		return returnObject;
 	}
 
+
+	var requireFunction = function(userAgent, OS, DEVICE, BROWSER) {
+		userAgent.osDetect = OS;
+		userAgent.deviceDetect = DEVICE;
+		userAgent.browserDetect = BROWSER;
+	}
+
+
 	// Node.js
-	if (typeof module !== 'undefined' && module.exports) {
+	if (NODE) {
+		requireFunction(userAgent, require('./lib/os'), require('./lib/device'), require('./lib/browser'));
 		module.exports = userAgent;
 	}
 	// AMD
-	else if (typeof define !== 'undefined' && define.amd) {
-		define([], function() {
+	else if (AMD) {
+		define(['./lib/os', './lib/device', './lib/browser'], function(OS, DEVICE, BROWSER) {
+			requireFunction(userAgent, OS, DEVICE, BROWSER);
+			console.log(userAgent);
 			return userAgent;
 		});
 	}
 	// CMD
-	else if (typeof define !== 'undefined' && define.cmd) {
-		define([], function(require, exports, module) {
+	else if (CMD) {
+		define(function(require, exports, module) {
+			requireFunction(userAgent, require('./lib/os'), require('./lib/device'), require('./lib/browser'));
 			module.exports = userAgent;
 		});
 	} else {
+		if (typeof USERAGENT_OS !== 'undefined') userAgent.osDetect = new USERAGENT_OS();
+		if (typeof USERAGENT_DEVICE !== 'undefined') userAgent.deviceDetect = new USERAGENT_DEVICE();
+		if (typeof USERAGENT_BROWSER !== 'undefined') userAgent.browserDetect = new USERAGENT_BROWSER();
 		window.USERAGENT = userAgent;
 	}
+
 
 })();
