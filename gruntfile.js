fe1574034bd262aafeba37844b570b4fec3e3a99
zsx <zsx@zsxsoft.com>
2015-02-18 13:30:18 +0800

    Grunt

--- gruntfile.js
+++ gruntfile.js
@@ -2,15 +2,41 @@
 
 module.exports = function(grunt) {
 	var packageJson = grunt.file.readJSON('package.json');
+	var encode = grunt.option('encode') || 'utf8';
+	var language = grunt.option('language') || 'node';
+	var doNotCompile = grunt.option("without-compile") || false;
+	language = language.toLowerCase();
 
 	grunt.initConfig({
 		pkg: packageJson,
+		copy: {
+			base: {
+				files: [{
+					src: ['lib/*.js', 'index.js'],
+					dest: 'dist/tmp/'
+				}]
+			}, 
+			asp: {
+				expand: true, 
+				src: "dist/*.js",
+				dest: "dist/", 
+				options: {
+					process: function (content, srcpath) {
+						console.log(srcpath);
+						return '<script language="javascript" runat="server">\n' + content + '\n</script>';
+					}
+				},
+				rename: function (dest, src) {
+					return src.replace(/\.js$/, '.asp');
+				}
+			}
+		},
 		concat: {
-			options: {
+			/*options: {
 				separator: ";"
-			},
+			},*/
 			dist: {
-				src: ['lib/*.js', 'index.js'],
+				src: ['dist/tmp/lib/*.js', 'dist/tmp/index.js'],
 				dest: 'dist/useragent.js'
 			}
 		},
@@ -21,17 +47,84 @@ module.exports = function(grunt) {
 				},
 				src: 'dist/useragent.js',
 				dest: 'dist/useragent.min.js',
-
 			}
 		},
+		replace: {
+			asp: {
+				src: ['dist/tmp/lib/*.js', 'dist/tmp/index.js'],
+				overwrite: true,
+				replacements: [{
+					from: '(function(root) {',
+					to: 'with (this) { (function() {'
+				}, {
+					from: /root\.(.+)=(.+)/,
+					to: '$1=$2'
+				}, {
+					from: /\}\)\(this\);$/,
+					to: "})(); }"
+				}, {
+					from: '["\']use strict["\']',
+					to: ""
+				}]
+			},
+			aspOutput: {
+				replacements: [{
+
+				}]
+			}
+		},
+		clean: {
+			build: {
+				src: [
+					"dist/tmp/",
+				]
+			}
+		},
+		transcoding: {
+			options: {
+				charset: encode
+			},
+			src: ['dist/*.js']
+		},
+		jshint: {
+			options: {
+				"-W084": true,  // if (rep = ret.ua.match())
+				'-W085': true   // ASP uses with for global
+			},
+			before: ['dist/tmp/*'],
+			concat: ['dist/useragent.js'],
+		}, 
+		nodeunit: {
+			all: ["test/test.js"]
+		}
 
 	});
 
+
+	grunt.loadNpmTasks('grunt-text-replace');
 	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-closurecompiler');
+	grunt.loadNpmTasks('grunt-transcoding');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
 
 	grunt.registerTask('default', 'useragent.js', function() {
-		var tasks = ['concat', 'closurecompiler'];
+		var tasks = ["nodeunit", "copy:base"];
+		if (language === "asp") tasks.push('replace:asp');
+		tasks.push("jshint:before", "concat", "jshint:concat");
+		if (!doNotCompile) {
+			tasks.push('closurecompiler');
+		}
+
+		tasks.push("transcoding");
+		if (language === "asp") {
+			tasks.push("copy:asp");
+		}
+		tasks.push("clean");
+
 		grunt.task.run(tasks);
 
 	});
